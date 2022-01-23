c590a8852e6e3318330dd503b354207cdd3495a1
BlocklingIII <pouit.aurele@gmail.com>
2022-01-23 20:18:09 +0100

    first commit

---
+++ BlockAPI-bungeecord/src/main/java/net/blocklingiii/blockapi/network/Main.java
@@ -0,0 +1,17 @@
+package net.blocklingiii.blockapi.network;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class Main extends Plugin
+{
+
+    @Override
+    public void onEnable()
+    {
+    }
+
+    @Override
+    public void onDisable()
+    {
+    }
+}
